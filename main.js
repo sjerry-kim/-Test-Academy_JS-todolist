42b0374b478ece530b9618fee44a50cca1a8f76e
jin hye <sjrry112@gmail.com>
2022-10-06 10:18:14 +0900

    221006_1st_Commit

--- main.js
+++ main.js
@@ -10,6 +10,10 @@ const bodyBackground = document.querySelector("body");
 bodyBackground.style.background = `url(./img/${imgList[RandomNum]})`;
 bodyBackground.style.backgroundSize = 'cover'; 
 
+// finished DOM, 변수 작성하기 : 카운트편
+const finshedCount = document.querySelector("#finished-p");
+let count = 0;
+
 // 투두리스트 만들기
 const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-input");
@@ -48,13 +52,20 @@ function addList(e){
             if(listCheckbox.checked){
                 listText.style.textDecorationLine = "line-through";
                 listText.style.color = "gray";
+                // finished 설정하기 : 카운트편
+                count++;
+                finshedCount.innerHTML = `finished : ${count}`;
             }else{
                 listText.style.textDecorationLine = "none";
                 listText.style.color = "ivory"
+                // finished 설정하기 : 카운트편
+                count--;
+                finshedCount.innerHTML = `finished : ${count}`;
             }
         }
 
-        // finished 설정하기
+        /*
+        // finished 설정하기 : 배열편 (deleteBtn 적용 안 되어 미흡함)
         listCheckbox.addEventListener("click",checkTodayList);
 
         function checkTodayList(){
@@ -65,10 +76,17 @@ function addList(e){
             const finishedP = document.querySelector("#finished-p");
             finishedP.innerHTML = `finished : ${seletChecklistCount}`
         }
-         
+        */
+        
         // 삭제버튼 설정하기
         deleteBtn.addEventListener("click",deleteList);
+        
         function deleteList(e){
+            // finished 설정하기 : 카운트편
+            if(e.target.parentNode.firstChild.checked){
+                count--;
+                finshedCount.innerHTML = `finished : ${count}`;
+            }
             e.target.parentNode.remove();
         }
 
